eeec2d6b0ebe0127717757a3638e64731238a036
Mrcll <linomarcel399@gmail.com>
2024-12-18 18:17:19 +0700

    Nampilin film di user dashboard

--- src/main/java/com/example/m08/User/PelangganController.java
+++ src/main/java/com/example/m08/User/PelangganController.java
@@ -4,21 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PelangganController {
 
     @Autowired
     private UserService userService;
-
-    @GetMapping("/dashboard")
-    public String dashboard() {
-        return "dashboard";
-    }
 
     @GetMapping("/register")
     public String registerView(Model model) {
