9b16adca61915b8addd02a63174c185332b2667e
bimoma <bimoadikoesoemo@gmail.com>
2024-12-29 11:52:58 +0700

    added top movie in dashboard so it can display top 3 most sold movies

--- src/main/java/com/example/m08/DashboardController.java
+++ src/main/java/com/example/m08/DashboardController.java
@@ -1,5 +1,7 @@
 package com.example.m08;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.m08.AdminMovie.Admin;
 import com.example.m08.AdminMovie.AdminService;
+import com.example.m08.Movie.Movie;
+import com.example.m08.Movie.MovieRepository;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class DashboardController {
 
+    @Autowired
+    private MovieRepository movieRepository;
+
     @GetMapping("/")
-    public String dashboard()
+    public String dashboard(Model model)
     {
+        List<Movie> movies = movieRepository.findTop3MostSoldMovies();
+        List<String> genres = movieRepository.getAllGenres();
+        model.addAttribute("movies", movies);
+        model.addAttribute("genres", genres);
         return "dashboard";
     }
 }
